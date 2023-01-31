ca1c4dcfb1ecd0e60d826b778f16ff87215a2477
Kamaljeet_Sahoo <kamaljeet@richpanel.com>
2023-02-01 01:08:22 +0530

    Products page

--- product/models.py
+++ product/models.py
@@ -6,6 +6,9 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to='product_images')
+    image = models.ImageField(upload_to='product_images', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
 
     
--- product/views.py
+++ product/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from .models import Product
+#from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("<h1>Hello Home page</h1>")
+    #return HttpResponse("<h1>Hello Home page</h1>")
+    p = Product.objects.all()
+    c = {
+        'name': 'KJ asdvasd',
+        'fruits': ['apple', 'orange', 'mango'],
+        'products': p,
+    }
+    return render(request, 'products.html', context=c)
